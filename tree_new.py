53f95e68c973541609a407fafffd2f859f782a03
atork <71847899+atork@users.noreply.github.com>
2023-12-11 22:13:35 +0100

    scuffed

---
+++ tree_new.py
@@ -0,0 +1,10 @@
+height = int(input("Enter the height of the tree: "))
+for i in range(0, height):
+    if i == 1:
+        for j in range(i):
+            print("*", end="")
+        print()
+    
+    
+
+
