9fc9bbdb0d8daee7744da062185fea3806b52d02
atork <71847899+atork@users.noreply.github.com>
2023-12-19 18:09:02 +0100

    Tutel added

--- test.py
+++ test.py
@@ -1,2 +1,18 @@
-print("Hello World",end=' ')
-print("1")
+import random
+import time
+import turtle
+
+from torch import rand
+tutel = turtle.Turtle()
+#tutel.speed(100)
+#for i in range(40):
+    #tutel.circle(100)
+    #tutel.left(10)
+#for i in range(25):
+    #tutel.left(13.7)
+    #tutel.circle(140)
+#time.sleep(10)
+tutel.turtlesize(100)
+for i in range(99):
+    tutel.forward(100)
+    tutel.left(random.randint(150,360))
