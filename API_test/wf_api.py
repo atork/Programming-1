1f0aceed1b256b3b32e04c8e5432e64720b9c89a
atork <71847899+atork@users.noreply.github.com>
2024-01-04 22:36:11 +0100

    added print out for the oldest 5 buy and sell ord.

--- API_test/wf_api.py
+++ API_test/wf_api.py
@@ -7,8 +7,14 @@ def jprint(obj):
 
 
 
-resp = requests.get('https://api.warframe.market/v1/items/primed_convulsion')
+resp = requests.get('https://api.warframe.market/v1/items/ash_prime_blueprint/orders')
 resp_dict = resp.json()
 
-print(resp_dict.get('page'))
+#prints out the oldest sell/buy-orders
+#jprint(str(resp_dict["payload"]["item"]["items_in_set"][0]["en"]["description"]))
+jprint(resp_dict["payload"]["orders"][0]["creation_date"])
+jprint(resp_dict["payload"]["orders"][1]["creation_date"])
+jprint(resp_dict["payload"]["orders"][2]["creation_date"])
+jprint(resp_dict["payload"]["orders"][3]["creation_date"])
+jprint(resp_dict["payload"]["orders"][4]["creation_date"])
 
