3bb32ebc42ecf0c13a7e8d453abb255184817872
atork <71847899+atork@users.noreply.github.com>
2024-01-06 01:51:59 +0100

    Out newest sell/buyorder

--- API_test/wf_api.py
+++ API_test/wf_api.py
@@ -1,20 +1,23 @@
-
 import requests
 import json
+from sympy import false, true
+
+resp = requests.get('https://api.warframe.market/v1/items/ash_prime_blueprint/orders')
+resp_dict = resp.json()
+bs_orders = resp_dict["payload"]["orders"]
+bs_orders_len = len(bs_orders)-1
+test = false
+
 def jprint(obj):
     text = json.dumps(obj, sort_keys=True, indent=4)
     print(text)
 
-
-
-resp = requests.get('https://api.warframe.market/v1/items/ash_prime_blueprint/orders')
-resp_dict = resp.json()
-
-#prints out the oldest sell/buy-orders
-#jprint(str(resp_dict["payload"]["item"]["items_in_set"][0]["en"]["description"]))
-jprint(resp_dict["payload"]["orders"][0]["creation_date"])
-jprint(resp_dict["payload"]["orders"][1]["creation_date"])
-jprint(resp_dict["payload"]["orders"][2]["creation_date"])
-jprint(resp_dict["payload"]["orders"][3]["creation_date"])
-jprint(resp_dict["payload"]["orders"][4]["creation_date"])
-
+#outputs newest available offer for ash blueprint
+while test == false:
+    if bs_orders[bs_orders_len]["user"]["status"] == "ingame":
+        print("Status:    ",bs_orders[bs_orders_len]["user"]["status"])
+        print("Price:     ",bs_orders[bs_orders_len]["platinum"])
+        print("Index num: ",bs_orders_len)
+        test = true
+    else:
+        bs_orders_len-=1
