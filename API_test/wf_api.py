366384496f20a777ca3ef2d7f6a28606c8c4c8f2
atork <71847899+atork@users.noreply.github.com>
2024-01-07 01:16:07 +0100

    added sellchecker

--- API_test/wf_api.py
+++ API_test/wf_api.py
@@ -1,3 +1,4 @@
+from turtle import done
 import requests
 import json
 from sympy import false, true
@@ -5,19 +6,30 @@ from sympy import false, true
 resp = requests.get('https://api.warframe.market/v1/items/ash_prime_blueprint/orders')
 resp_dict = resp.json()
 bs_orders = resp_dict["payload"]["orders"]
-bs_orders_len = len(bs_orders)-1
+s_orders = []
 test = false
-
+#json printer
 def jprint(obj):
     text = json.dumps(obj, sort_keys=True, indent=4)
     print(text)
 
+#sellorder checker
+def sellchecker(list,slist):
+    for x in list:
+        if x["order_type"] == "sell":
+            slist.append(x)
+            print("hello")
+           
+sellchecker(bs_orders,s_orders)
+s_orders_len = len(s_orders)-1      
+
 #outputs newest available offer for ash blueprint
 while test == false:
-    if bs_orders[bs_orders_len]["user"]["status"] == "ingame":
-        print("Status:    ",bs_orders[bs_orders_len]["user"]["status"])
-        print("Price:     ",bs_orders[bs_orders_len]["platinum"])
-        print("Index num: ",bs_orders_len)
+    if s_orders[s_orders_len]["user"]["status"] == "ingame":
+        print("Status:    ",s_orders[s_orders_len]["user"]["status"])
+        print("Price:     ",s_orders[s_orders_len]["platinum"])
+        print("Index num: ",s_orders_len)
         test = true
     else:
-        bs_orders_len-=1
+        s_orders_len-=1
+
