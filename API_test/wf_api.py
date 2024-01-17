e239d9843b11f141d4250940f0d7cd5dd5f70320
atork <71847899+atork@users.noreply.github.com>
2024-01-17 23:48:32 +0100

    added price sorter, now works as intended

--- API_test/wf_api.py
+++ API_test/wf_api.py
@@ -8,6 +8,7 @@ resp_dict = resp.json()
 bs_orders = resp_dict["payload"]["orders"]
 s_orders = []
 test = false
+
 #json printer
 def jprint(obj):
     text = json.dumps(obj, sort_keys=True, indent=4)
@@ -18,22 +19,27 @@ def sellchecker(list,slist):
     for x in list:
         if x["order_type"] == "sell":
             slist.append(x)
-            
-#price sorter
+#price sorter after ky with lambda(no idea how lambda works)
 def price_sort_asc(list):
-    for x in list:
-        x+1
+    price_l=sorted(list, key=lambda d:d ["platinum"],reverse=True)
+    return price_l
+    
 
-
+#list of of all buy and sell sorted into a sellorder var
 sellchecker(bs_orders,s_orders)
-s_orders_len = len(s_orders)-1      
 
-#outputs newest available offer
+#sell orders sorted by price asc and lenght for ingame checker
+s_p_orders=price_sort_asc(s_orders)
+s_p_orders_len = len(s_p_orders)-1
+
+#//to be rewritten as function
+#outputs cheapest/newest/online sellorder
 while test == false:
-    if s_orders[s_orders_len]["user"]["status"] == "ingame":
-        print("Status:    ",s_orders[s_orders_len]["user"]["status"])
-        print("Price:     ",s_orders[s_orders_len]["platinum"])
-        print("Index num: ",s_orders_len)
+    if s_p_orders[s_p_orders_len]["user"]["status"] == "ingame":
+        print("Status:    ",s_p_orders[s_p_orders_len]["user"]["status"])
+        print("Price:     ",s_p_orders[s_p_orders_len]["platinum"])
+        print("Index num: ",s_p_orders_len)
+        print("Name:     ",s_p_orders[s_p_orders_len]["user"]["ingame_name"])
         test = true
     else:
-        s_orders_len-=1
+        s_p_orders_len-=1
