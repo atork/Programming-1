b095faedfd995d98a1ac06f42a3bc8b3e0667e1c
atork <71847899+atork@users.noreply.github.com>
2024-01-21 23:53:41 +0100

    increased security

--- pass_gen.py
+++ pass_gen.py
@@ -4,7 +4,7 @@ import string
 
 #Function to generate a random password between 8 and 16 characters long
 def generate_password():
-    length = random.randint(8, 16)
+    length = random.randint(16, 32)
     return ''.join(random.choices(string.ascii_letters + string.digits, k=length))
 
 #Function to print the generated password
