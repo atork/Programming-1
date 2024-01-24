81eeffe2e8b2637ce2e04c58d4e28cb3d502c20f
atork <71847899+atork@users.noreply.github.com>
2024-01-24 21:03:43 +0100

    added depacker for payload

--- API_test/wf_api.py
+++ API_test/wf_api.py
@@ -2,18 +2,14 @@ import requests
 import json
 from sympy import true,false
 
-input = input("Item name: ")
-resp = requests.get('https://api.warframe.market/v1/items/'+input+'/orders')
-resp_dict = resp.json()
-bs_orders = resp_dict["payload"]["orders"]
-s_orders = []
-test = false
+
 
 #json printer
 def jprint(obj):
     text = json.dumps(obj, sort_keys=True, indent=4)
     print(text)
-
+def depacker(dict):
+    return dict["payload"]["orders"]
 #sellorder checker
 def sellchecker(list,slist):
     for x in list:
@@ -29,18 +25,3 @@ def online(list):
     for x in list:
         if x["user"]["status"]=="ingame":
             return x
-
-
-#//programstart  
-#list of of all buy and sell sorted into a sellorder var
-sellchecker(bs_orders,s_orders)
-
-#sell orders sorted by price asc and lenght for ingame checker
-s_p_orders=price_sort_asc(s_orders)
-
-#puts online orders in x
-final_order=online(s_p_orders)
-
-jprint(final_order)
-
-
