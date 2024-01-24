cf1162f1f0460d82b02e8febe44d60ce88bd63d8
atork <71847899+atork@users.noreply.github.com>
2024-01-24 21:14:17 +0100

    attempt to get full set

---
+++ API_test/wf_api_set_calc.py
@@ -0,0 +1,29 @@
+import wf_api
+import requests
+import json
+from sympy import true,false
+
+input = input("Warframe name: ")
+fullnam= input+"_prime"
+
+wf_blueprint=fullnam+"_blueprint"
+wf_sys=fullnam+"_systems"
+wf_neuro=fullnam+"_neuroptics"
+wf_chassie=fullnam+"_chassie"
+
+resp = requests.get('https://api.warframe.market/v1/items/'+wf_blueprint+'/orders')
+blue_resp_dict = resp.json()
+blue_sb=wf_api.depacker(blue_resp_dict)
+resp = requests.get('https://api.warframe.market/v1/items/'+wf_sys+'/orders')
+sys_resp_dict = resp.json()
+sys_sb=wf_api.depacker(sys_resp_dict)
+resp = requests.get('https://api.warframe.market/v1/items/'+wf_neuro+'/orders')
+neuro_resp_dict = resp.json()
+neuro_sb=wf_api.depacker(neuro_resp_dict)
+resp = requests.get('https://api.warframe.market/v1/items/'+wf_chassie+'/orders')
+chassie_resp_dict = resp.json()
+chassie_sb=wf_api.depacker(chassie_resp_dict)
+
+blue_s=None
+wf_api.sellchecker(blue_sb,blue_s)
+wf_api.jprint(blue_s)
