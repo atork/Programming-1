701f9f99a2798d9ee8cb00f8113080372549ad92
atork <71847899+atork@users.noreply.github.com>
2024-01-28 20:05:05 +0100

    chassis naming error fixed

--- API_test/wf_api_set_calc.py
+++ API_test/wf_api_set_calc.py
@@ -9,7 +9,7 @@ fullnam= input+"_prime"
 wf_blueprint=fullnam+"_blueprint"
 wf_sys=fullnam+"_systems"
 wf_neuro=fullnam+"_neuroptics"
-wf_chassie=fullnam+"_chassie"
+wf_chassis=fullnam+"_chassis"
 
 resp = requests.get('https://api.warframe.market/v1/items/'+wf_blueprint+'/orders')
 blue_resp_dict = resp.json()
@@ -20,12 +20,6 @@ sys_sb=wf_api.depacker(sys_resp_dict)
 resp = requests.get('https://api.warframe.market/v1/items/'+wf_neuro+'/orders')
 neuro_resp_dict = resp.json()
 neuro_sb=wf_api.depacker(neuro_resp_dict)
-resp = requests.get('https://api.warframe.market/v1/items/'+wf_chassie+'/orders')
-chassie_resp_dict = resp.json()
-#depacker for chassie not working key error???
-chassie_sb=wf_api.depacker(chassie_resp_dict)
-
-
-blue_s=[]
-wf_api.sellchecker(blue_sb,blue_s)
-wf_api.jprint(blue_s)
+resp = requests.get('https://api.warframe.market/v1/items/'+wf_chassis+'/orders')
+chassis_resp_dict = resp.json()
+chassie_sb=wf_api.depacker(chassis_resp_dict)
