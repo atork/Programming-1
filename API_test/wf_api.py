511acc69396aa1625896f0bbe9b65ee1b1ca4133
atork <71847899+atork@users.noreply.github.com>
2024-01-28 20:35:47 +0100

    updated library with get price function

--- API_test/wf_api.py
+++ API_test/wf_api.py
@@ -15,13 +15,16 @@ def sellchecker(list,slist):
     for x in list:
         if x["order_type"] == "sell":
             slist.append(x)
-#price sorter after ky with lambda(no idea how lambda works)
+#price sorter after key with lambda(no idea how lambda works)
 def price_sort_asc(list):
     price_l=sorted(list, key=lambda d:d ["platinum"])
     return price_l
 #ingame sorter
-def online(list):
+def ingame(list):
     lenghtl=len(list)
     for x in list:
         if x["user"]["status"]=="ingame":
             return x
+#price return
+def price(x):
+    return x["platinum"]
